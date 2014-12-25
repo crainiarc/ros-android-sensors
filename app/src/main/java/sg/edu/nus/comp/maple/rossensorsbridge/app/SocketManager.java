8a967ee90c7bad60a43e0eed94c0cc59f3dc11fe
Keng Kiat Lim <kkin@crnrc.com>
2014-12-26 04:37:22 +0800

    Added stubs for socket class

--- app/src/main/java/sg/edu/nus/comp/maple/rossensorsbridge/app/SocketManager.java
+++ app/src/main/java/sg/edu/nus/comp/maple/rossensorsbridge/app/SocketManager.java
@@ -10,6 +10,7 @@ public class SocketManager implements Runnable {
     private Socket mSocket;
     private String mHost;
     private int mPort;
+    private boolean mIsSocketOpen;
     private PrintWriter mOutPrintWriter;
     private DataOutputStream mDataOutputStream;
     private BufferedReader mInBufferedReader;
@@ -21,6 +22,7 @@ public class SocketManager implements Runnable {
 
     public void startConnection() throws IOException {
         this.mSocket = new Socket(this.mHost, this.mPort);
+        this.mIsSocketOpen = true;
         this.mOutPrintWriter = new PrintWriter(this.mSocket.getOutputStream(), true);
         this.mDataOutputStream = new DataOutputStream(this.mSocket.getOutputStream());
         this.mInBufferedReader = new BufferedReader(new InputStreamReader(this.mSocket.getInputStream()));
@@ -31,10 +33,27 @@ public class SocketManager implements Runnable {
         this.mDataOutputStream.close();
         this.mInBufferedReader.close();
         this.mSocket.close();
+        this.mIsSocketOpen = false;
     }
 
     @Override
     public void run() {
+        while (this.mIsSocketOpen) {
+            try {
+                String request = this.mInBufferedReader.readLine();
+                this.sendSensorData();
+                this.sendImageData();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
+    private void sendSensorData() {
+        this.mOutPrintWriter.println("13");
+        this.mOutPrintWriter.println("Hello, world!");
+    }
+
+    private void sendImageData() {
     }
 }
