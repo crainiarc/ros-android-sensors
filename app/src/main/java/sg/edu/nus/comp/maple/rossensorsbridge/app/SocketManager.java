21c2bd194096af21fcd30787c1a669f3413dd512
Keng Kiat Lim <kkin@crnrc.com>
2014-12-28 17:39:29 +0800

    SocketManager can now connect to computer

--- app/src/main/java/sg/edu/nus/comp/maple/rossensorsbridge/app/SocketManager.java
+++ app/src/main/java/sg/edu/nus/comp/maple/rossensorsbridge/app/SocketManager.java
@@ -7,10 +7,11 @@ import java.net.Socket;
  * Created by Keng Kiat Lim on 12/25/14.
  */
 public class SocketManager implements Runnable {
+
     private Socket mSocket;
     private String mHost;
     private int mPort;
-    private boolean mIsSocketOpen;
+
     private PrintWriter mOutPrintWriter;
     private DataOutputStream mDataOutputStream;
     private BufferedReader mInBufferedReader;
@@ -20,32 +21,19 @@ public class SocketManager implements Runnable {
         this.mPort = port;
     }
 
-    public void startConnection() throws IOException {
-        this.mSocket = new Socket(this.mHost, this.mPort);
-        this.mIsSocketOpen = true;
-        this.mOutPrintWriter = new PrintWriter(this.mSocket.getOutputStream(), true);
-        this.mDataOutputStream = new DataOutputStream(this.mSocket.getOutputStream());
-        this.mInBufferedReader = new BufferedReader(new InputStreamReader(this.mSocket.getInputStream()));
-    }
-
-    public void stopConnection() throws IOException {
-        this.mOutPrintWriter.close();
-        this.mDataOutputStream.close();
-        this.mInBufferedReader.close();
-        this.mSocket.close();
-        this.mIsSocketOpen = false;
-    }
-
     @Override
     public void run() {
-        while (this.mIsSocketOpen) {
-            try {
-                String request = this.mInBufferedReader.readLine();
+        try {
+            this.startConnection();
+            while (true) {
+                this.mInBufferedReader.readLine();
                 this.sendSensorData();
                 this.sendImageData();
-            } catch (IOException e) {
-                e.printStackTrace();
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            this.stopConnection();
         }
     }
 
@@ -55,5 +43,23 @@ public class SocketManager implements Runnable {
     }
 
     private void sendImageData() {
+    }
+
+    private void startConnection() throws IOException {
+        this.mSocket = new Socket(this.mHost, this.mPort);
+        this.mOutPrintWriter = new PrintWriter(this.mSocket.getOutputStream(), true);
+        this.mDataOutputStream = new DataOutputStream(this.mSocket.getOutputStream());
+        this.mInBufferedReader = new BufferedReader(new InputStreamReader(this.mSocket.getInputStream()));
+    }
+
+    private void stopConnection() {
+        this.mOutPrintWriter.close();
+        try {
+            this.mDataOutputStream.close();
+            this.mInBufferedReader.close();
+            this.mSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
