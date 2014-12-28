f51eb812a5c2966741e932b146f221b44b2f31cf
Keng Kiat Lim <kkin@crnrc.com>
2014-12-29 00:05:21 +0800

    SensorData object now outputs to JSON format

--- app/src/main/java/sg/edu/nus/comp/maple/rossensorsbridge/app/SensorData.java
+++ app/src/main/java/sg/edu/nus/comp/maple/rossensorsbridge/app/SensorData.java
@@ -1,6 +1,9 @@
 package sg.edu.nus.comp.maple.rossensorsbridge.app;
 
 import android.hardware.Sensor;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by Keng Kiat Lim on 12/27/14.
@@ -17,6 +20,27 @@ public class SensorData {
         this.mSensorStringType = sensor.getName();
         this.mValues = values;
         this.mAccuracy = accuracy;
+    }
+
+    public JSONObject toJSONObject() {
+        JSONObject jsonObject = new JSONObject();
+        try {
+            JSONArray jsonValues = new JSONArray();
+            for (float value : this.mValues) {
+                jsonValues.put(value);
+            }
+
+            jsonObject.put("sensorIntType", this.mSensorType);
+            jsonObject.put("sensorStringType", this.mSensorStringType);
+            jsonObject.put("accuracy", this.mAccuracy);
+            jsonObject.put("values", jsonValues);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+
+        } finally {
+            return jsonObject;
+        }
     }
 
     public int getSensorType() {
